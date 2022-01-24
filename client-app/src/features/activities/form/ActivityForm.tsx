ff1077e175736f5ee91673849428710befd580c9
razvanr91 <razvan.roman91@outlook.com>
2022-01-24 14:47:01 +0000

    Add activity details to form or make form register new data

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
-import React, { Fragment } from "react";
-import { Button, Card, Container, Form } from "react-bootstrap";
+import React, { ChangeEvent, Fragment, useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
 import { Activity } from "../../../app/models/activity";
 
 interface Props {
@@ -8,33 +8,73 @@ interface Props {
 }
 
 export default function ActivityForm({ activity, closeForm }: Props) {
+	const initialState = activity ?? {
+		id: "",
+		title: "",
+		category: "",
+		description: "",
+		date: "",
+		city: "",
+		venue: "",
+	};
+
+	const [newActivity, setNewActivity] = useState(initialState);
+
+	function handleSubmit() {
+		console.log(newActivity);
+	}
+
+	function handleInputChange(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+		const { name, value } = e.target;
+
+		setNewActivity({ ...newActivity, [name]: value });
+	}
+
 	return (
 		<Fragment>
 			<Container className="bg-white">
-				<Form>
+				<Form onSubmit={handleSubmit} autoComplete="off">
 					<Form.Group>
 						<Form.Label className="mt-2">Title</Form.Label>
-						<Form.Control type="text" />
+						<Form.Control placeholder="What is the activity?" type="text" value={newActivity.title} name="title" onChange={handleInputChange} />
 					</Form.Group>
 					<Form.Group>
 						<Form.Label className="mt-2">Description</Form.Label>
-						<Form.Control as="textarea" />
+						<Form.Control
+							placeholder="Describe the activity"
+							as="textarea"
+							value={newActivity.description}
+							name="description"
+							onChange={handleInputChange}
+						/>
 					</Form.Group>
 					<Form.Group>
 						<Form.Label className="mt-2">Category</Form.Label>
-						<Form.Control type="text" />
+						<Form.Control placeholder="e.g. drinks, culture" type="text" value={newActivity.category} name="category" onChange={handleInputChange} />
 					</Form.Group>
 					<Form.Group>
 						<Form.Label className="mt-2">Date</Form.Label>
-						<Form.Control type="text" />
+						<Form.Control placeholder="When?" type="text" value={newActivity.date} name="date" onChange={handleInputChange} />
 					</Form.Group>
 					<Form.Group>
 						<Form.Label className="mt-2">City</Form.Label>
-						<Form.Control type="text" />
+						<Form.Control
+							placeholder="What city is the activity going to take place in?"
+							type="text"
+							value={newActivity.city}
+							name="city"
+							onChange={handleInputChange}
+						/>
 					</Form.Group>
 					<Form.Group>
 						<Form.Label className="mt-2">Venue</Form.Label>
-						<Form.Control type="text" />
+						<Form.Control
+							placeholder="What is the location of the event?"
+							type="text"
+							value={newActivity.venue}
+							name="venue"
+							onChange={handleInputChange}
+						/>
 					</Form.Group>
 					<Button className="my-3 float-end" variant="primary" type="submit">
 						Submit
