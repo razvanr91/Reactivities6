a033130507c77cd2371c2a59a9c516c0e718e696
razvanr91 <razvan.roman91@outlook.com>
2022-01-24 20:46:43 +0000

    Create axios agent

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Fragment, useEffect, useState } from "react";
-import axios from "axios";
 import { Container } from "react-bootstrap";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { v4 as uuid } from "uuid";
+import agent from "../api/agent";
 
 function App() {
 	const [activities, setActivities] = useState<Activity[]>([]);
@@ -13,9 +13,8 @@ function App() {
 	const [editMode, setEditMode] = useState(false);
 
 	useEffect(() => {
-		axios.get<Activity[]>("https://localhost:5001/api/activities").then((response) => {
-			console.log(response);
-			setActivities(response.data);
+		agent.Activities.list().then((response) => {
+			setActivities(response);
 		});
 	}, []);
 
